acf98f302f1f9a380370e00d0e6f4ddf4761894d
Hendrix Schmidt <hendrixschmidt@hotmail.com>
2024-11-25 15:11:35 -0300

    Adicionando validações e disparando exeções. Refatoração para deixar de usar @Query no repository

--- src/main/java/com/br/pessoas/infra/dataProvider/repository/impl/PersonRepositoryImpl.java
+++ src/main/java/com/br/pessoas/infra/dataProvider/repository/impl/PersonRepositoryImpl.java
@@ -2,11 +2,15 @@ package com.br.pessoas.infra.dataProvider.repository.impl;
 
 import com.br.pessoas.infra.dataProvider.repository.PersonRepository;
 import com.br.pessoas.infra.dataProvider.repository.model.PersonModel;
+import com.br.pessoas.useCase.exception.PersonRecoveryException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.domain.Example;
+import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class PersonRepositoryImpl {
@@ -18,10 +22,26 @@ public class PersonRepositoryImpl {
     }
 
     public PersonModel get(Long id) {
-        return repository.findById(id).orElseThrow(() -> new RuntimeException("Pessoa não localizada."));
+        return repository.findById(id).orElseThrow(() -> new PersonRecoveryException("Pessoa não encontrada."));
     }
 
     public List<PersonModel> getAll() {
         return repository.findAll();
+    }
+
+    public Optional<PersonModel> findByCpf(final String cpf) {
+        final PersonModel personModel = new PersonModel();
+        personModel.setCpf(cpf);
+        final Example<PersonModel> example = Example.of(personModel);
+
+        return repository.findBy(example, FluentQuery.FetchableFluentQuery::first);
+    }
+
+    public boolean existsByCpf(final String cpf){
+        final PersonModel personModel = new PersonModel();
+        personModel.setCpf(cpf);
+        final Example<PersonModel> example = Example.of(personModel);
+
+        return repository.exists(example);
     }
 }
